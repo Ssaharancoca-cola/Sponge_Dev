13389b83912c88f136b5aae94562cc2539166564
Surendra Saharan <ssaharan@coca-cola.com>
2023-11-07 06:50:36 +0000

    Approval

---
+++ Sponge/Controllers/ApprovalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sponge.Controllers
+{
+    public class ApprovalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyApproval()
+        {
+            return View();
+        }
+    }
+}
