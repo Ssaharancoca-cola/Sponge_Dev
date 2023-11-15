6fb5aa0d6ff7e6efc182b1c74fd1127a0ce3345c
Surendra Saharan <ssaharan@coca-cola.com>
2023-11-15 17:22:13 +0000

    Incoming changes of setup

---
+++ Sponge/Controllers/SearchTemplateController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sponge.Common;
+using Sponge.Models;
+using System.Diagnostics;
+
+namespace Sponge.Controllers
+{
+    [AccessFilters]
+    [SessionTimeOut]
+    public class SearchTemplateController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public SearchTemplateController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SearchTemplate()
+        {
+            return View();
+        }
+
+        public IActionResult ManualSend()
+        {
+            return View("Views\\SearchTemplate\\ManualSendResend.cshtml");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       
+    }
+}
