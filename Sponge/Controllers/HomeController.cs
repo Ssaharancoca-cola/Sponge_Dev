e025f113c6ea10a226a2b0641e3f822e9dadda99
Annu <Z12079@zwdmyaa0002>
2024-01-31 05:52:00 +0000

    Error Page - Home

--- Sponge/Controllers/HomeController.cs
+++ Sponge/Controllers/HomeController.cs
@@ -49,7 +49,7 @@ namespace Sponge.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("~/Pages/ErrorPage.cshtml");
         }
         public ViewResult SessionTimeOut()
         {
