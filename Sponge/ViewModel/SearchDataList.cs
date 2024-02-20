297d9b10b9baeaafe8a9a302c9549b4fb015ee60
Bharat Singh <bharasingh@coca-cola.com>
2024-02-20 05:39:18 +0000

    Manual Generate Date bug fix + Traoling and leading spaces bug fix

--- Sponge/ViewModel/SearchDataList.cs
+++ Sponge/ViewModel/SearchDataList.cs
@@ -5,7 +5,7 @@
         public int ConfigId { get; set; }
         public int SubjectAreaId { get; set; }
         public string SubjectAreaName { get; set; }
-        public DateTime? EffectiveDate { get; set;}
+        public DateTime? EffectiveTo { get; set;}
         public DateTime? EffectiveFrom { get; set;}
         public string AssignedUser { get; set;}
         public string Active { get; set;}
