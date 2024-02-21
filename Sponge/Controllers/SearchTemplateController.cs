d8cf17d3a48f6530dd30d135f8eb9aec835e0674
Bharat Singh <bharasingh@coca-cola.com>
2024-02-21 05:48:50 +0000

    Bug Fixes

--- Sponge/Controllers/SearchTemplateController.cs
+++ Sponge/Controllers/SearchTemplateController.cs
@@ -32,20 +32,20 @@ namespace Sponge.Controllers
             int RoleID = 0;
             if (role != null)
             {
-                 RoleID = (int)role.ROLE_ID;
+                RoleID = (int)role.ROLE_ID;
             }
-            if((RoleID == 5 )|| (RoleID == 3))
+            if ((RoleID == 5) || (RoleID == 3))
             {
-                 subfunctions = sPONGE_Context.SPG_SUBFUNCTION.ToList();
+                subfunctions = sPONGE_Context.SPG_SUBFUNCTION.ToList();
             }
             else
             {
-                 subfunctions = (from subFunc in sPONGE_Context.SPG_SUBFUNCTION
-                                   join userFunc in sPONGE_Context.SPG_USERS_FUNCTION on subFunc.SUBFUNCTION_ID equals userFunc.SUB_FUNCTION_ID
-                                   where userFunc.USER_ID == userName[1] && userFunc.ACTIVE_FLAG == "Y"
-                                   select  subFunc).Distinct().ToList();
+                subfunctions = (from subFunc in sPONGE_Context.SPG_SUBFUNCTION
+                                join userFunc in sPONGE_Context.SPG_USERS_FUNCTION on subFunc.SUBFUNCTION_ID equals userFunc.SUB_FUNCTION_ID
+                                where userFunc.USER_ID == userName[1] && userFunc.ACTIVE_FLAG == "Y"
+                                select subFunc).Distinct().ToList();
             }
-            return View(subfunctions);           
+            return View(subfunctions);
         }
         public JsonResult GetSubjectAreas(int subFunctionId)
         {
@@ -58,9 +58,9 @@ namespace Sponge.Controllers
             {
                 RoleID = (int)role.ROLE_ID;
             }
-            if ((RoleID == 5) ||( RoleID==3))
+            if ((RoleID == 5) || (RoleID == 3))
             {
-                subjectArea = sPONGE_Context.SPG_SUBJECTAREA.Where(x=> x.SUBFUNCTION_ID == subFunctionId).ToList();
+                subjectArea = sPONGE_Context.SPG_SUBJECTAREA.Where(x => x.SUBFUNCTION_ID == subFunctionId).ToList();
             }
             else
             {
@@ -83,13 +83,13 @@ namespace Sponge.Controllers
             {
                 RoleID = (int)role.ROLE_ID;
             }
-            if ((RoleID == 5) || RoleID==3)
+            if ((RoleID == 5) || RoleID == 3)
             {
                 userlist = (from SubjectArea in sPONGE_Context.SPG_SUBJECTAREA
-                               join userconfig in sPONGE_Context.SPG_CONFIGURATION on SubjectArea.SUBJECTAREA_ID equals userconfig.SUBJECTAREA_ID
+                            join userconfig in sPONGE_Context.SPG_CONFIGURATION on SubjectArea.SUBJECTAREA_ID equals userconfig.SUBJECTAREA_ID
                             join users in sPONGE_Context.SPG_USERS on userconfig.USER_ID equals users.USER_ID
                             where SubjectArea.SUBJECTAREA_ID == subjectAreaId
-                               select users).ToList();
+                            select users).ToList();
             }
             else
             {
@@ -104,13 +104,13 @@ namespace Sponge.Controllers
             string[] userName = User.Identity.Name.Split(new[] { "\\" }, StringSplitOptions.None);
             SPONGE_Context sponge_context = new();
             List<SearchDataList> SearchConfgData = new();
-            var role = sponge_context.SPG_USERS_FUNCTION.FirstOrDefault(x => x.USER_ID == userName[1] && (x.ROLE_ID ==5 || x.ROLE_ID==3));
+            var role = sponge_context.SPG_USERS_FUNCTION.FirstOrDefault(x => x.USER_ID == userName[1] && (x.ROLE_ID == 5 || x.ROLE_ID == 3));
             var AdminRoleId = System.Configuration.ConfigurationManager.AppSettings["AdminRoleId"];
 
             // Check if the user has selected a date (other than the default date)
             bool isDateFromSelected = dateFrom != DateTime.MinValue;
             bool isDateToSelected = dateTo != DateTime.MinValue;
-
+            bool isActiveSelected = active == "Y" ? true : false;
 
             int RoleID = 0;
             if (role != null)
@@ -123,7 +123,7 @@ namespace Sponge.Controllers
                                    join u in sponge_context.SPG_USERS on conf.USER_ID equals u.USER_ID
                                    join sa in sponge_context.SPG_SUBJECTAREA on conf.SUBJECTAREA_ID equals sa.SUBJECTAREA_ID
                                    join sf in sponge_context.SPG_SUBFUNCTION on sa.SUBFUNCTION_ID equals sf.SUBFUNCTION_ID
-                                   where ( sf.SUBFUNCTION_ID == subFunctionId)
+                                   where (sf.SUBFUNCTION_ID == subFunctionId)
 
                                     && (subjectAreaId == 0 || sa.SUBJECTAREA_ID == subjectAreaId)
                                      && (assignToUser == "0" || conf.USER_ID == assignToUser)
@@ -139,7 +139,13 @@ namespace Sponge.Controllers
                                        EffectiveFrom = conf.EFFECTIVE_FROM,
                                        EffectiveTo = conf.EFFECTIVE_TO,
                                        ManualSendResendUrl = conf.ACTIVE_FLAG == null ? "In Progress" : conf.ACTIVE_FLAG == "N" ? "Inactive" : "Generate Template"
-                                    }).Distinct().ToList();
+                                   }).Distinct().ToList();
+                // To send data based on active status
+                //if(active != null)
+                //{
+                //    SearchConfgData = SearchDataOnStatus(subjectAreaId,subFunctionId, active);
+                //}
+
                 if (isDateFromSelected && !isDateToSelected)
                 {
                     // Only dateFrom is selected
@@ -155,7 +161,7 @@ namespace Sponge.Controllers
                     // Both dateFrom and dateTo are selected
                     SearchConfgData = SearchConfgData.Where(x =>
                         (x.EffectiveFrom == null || x.EffectiveFrom >= dateFrom) &&
-                        ( x.EffectiveTo <= dateTo)).ToList();
+                        (x.EffectiveTo <= dateTo)).ToList();
                 }
 
 
@@ -166,10 +172,10 @@ namespace Sponge.Controllers
                                    join u in sponge_context.SPG_USERS on conf.USER_ID equals u.USER_ID
                                    join sa in sponge_context.SPG_SUBJECTAREA on conf.SUBJECTAREA_ID equals sa.SUBJECTAREA_ID
                                    join sf in sponge_context.SPG_SUBFUNCTION on sa.SUBFUNCTION_ID equals sf.SUBFUNCTION_ID
-                                   where ( sf.SUBFUNCTION_ID == subFunctionId)
+                                   where (sf.SUBFUNCTION_ID == subFunctionId)
 
                                     && (subjectAreaId == 0 || sa.SUBJECTAREA_ID == subjectAreaId)
-                                     && ( (conf.APPROVER_ID == userName[1] || (conf.USER_ID== userName[1])))
+                                     && ((conf.APPROVER_ID == userName[1] || (conf.USER_ID == userName[1])))
                  && (active == null || conf.ACTIVE_FLAG == active)
                                    select new SearchDataList
                                    {
@@ -182,6 +188,12 @@ namespace Sponge.Controllers
                                        EffectiveFrom = conf.EFFECTIVE_FROM,
                                        ManualSendResendUrl = conf.ACTIVE_FLAG == null ? "In Progress" : conf.ACTIVE_FLAG == "N" ? "Inactive" : "Generate Template"
                                    }).Distinct().ToList();
+
+                // To send data based on active status
+                if (active != null)
+                {
+                    SearchConfgData = SearchDataOnStatus(subjectAreaId, subFunctionId, active);
+                }
                 if (isDateFromSelected && !isDateToSelected)
                 {
                     // Only dateFrom is selected
@@ -202,12 +214,50 @@ namespace Sponge.Controllers
             }
             return Json(SearchConfgData);
         }
-        
+
+        //public List<SearchDataList> SearchDataOnStatus(int subjectAreaId, int subFunctionId, string Status)
+        //{
+        //    SPONGE_Context sponge_context = new();
+        //    try
+        //    {
+        //        var query = (from conf in sponge_context.SPG_CONFIGURATION
+        //                           join u in sponge_context.SPG_USERS on conf.USER_ID equals u.USER_ID
+        //                           join sa in sponge_context.SPG_SUBJECTAREA on conf.SUBJECTAREA_ID equals sa.SUBJECTAREA_ID
+        //                           join sf in sponge_context.SPG_SUBFUNCTION on sa.SUBFUNCTION_ID equals sf.SUBFUNCTION_ID
+        //                           where (sf.SUBFUNCTION_ID == subFunctionId)
+
+        //                            && (subjectAreaId == 0 || sa.SUBJECTAREA_ID == subjectAreaId)
+        //                            && (Status == null || conf.ACTIVE_FLAG == Status)
+        //                            &&(Status == null || sa.ACTIVE_FLAG == Status)
+
+
+        //                           select new SearchDataList
+        //                           {
+        //                               ConfigId = conf.CONFIG_ID,
+        //                               SubjectAreaId = sa.SUBJECTAREA_ID,
+        //                               SubjectAreaName = sa.SUBJECTAREA_NAME,
+        //                               AssignedUser = u.Name,
+        //                               Active = (conf.ACTIVE_FLAG == null) ? "" : (conf.ACTIVE_FLAG == "Y" ? "Yes" : "No"),
+        //                               EffectiveFrom = conf.EFFECTIVE_FROM,
+        //                               EffectiveTo = conf.EFFECTIVE_TO,
+        //                               ManualSendResendUrl = conf.ACTIVE_FLAG == null ? "In Progress" : conf.ACTIVE_FLAG == "N" ? "Inactive" : "Generate Template"
+        //                           }).Distinct().ToList();
+
+        //        return query;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("An error occurred: " + ex.Message);
+
+        //        // Returning an empty list or null, depending on the desired behavior:
+        //        return new List<SearchDataList>(); // or return null;
+        //    }
+        //}
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-       
+
     }
 }
